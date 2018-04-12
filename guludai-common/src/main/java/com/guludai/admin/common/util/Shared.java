7d36ab1344f3ad2f640800711886cea8f308509a
heqiao <qiao.he@taocandai.com>
2018-04-12 10:37:08 +0800

    初始化项目

---
+++ guludai-common/src/main/java/com/guludai/admin/common/util/Shared.java
@@ -0,0 +1,9 @@
+package com.guludai.admin.common.util;
+
+class Shared {
+    static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    static char pad = '=';
+
+    Shared() {
+    }
+}
